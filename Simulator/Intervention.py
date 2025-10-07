fe765bf3d2c1f5fe5d1db9950ef0a544ae08bf3b
Elijah Merrill <elmerrill1@icloud.com>
2025-10-07 14:16:11 -0400

    Update Intervention.py

--- Simulator/Intervention.py
+++ Simulator/Intervention.py
@@ -2,15 +2,15 @@ from EnumeratedTypes import State, InterventionType
 import random
 from Population import Population
 
-# Different types of interventions to control the spread of the virus.
-# Can either vaccinate a portion of the population or quarantine infected individuals.
+''' Different types of interventions to control the spread of the virus.
+Can either vaccinate a portion of the population or quarantine infected individuals.'''
 
 class Intervention:
-    # Initialize with the type of intervention.
+    ''' Initialize with the type of intervention.'''
     def __init__(self, intervention_type: InterventionType):
         self.type = intervention_type
     
-    # Execute the intervention type on the given population.
+    ''' Execute the intervention type on the given population.'''
     def execute(self, population: Population):
         if self.type == InterventionType.VACCINE:
             for person in random.sample(population.population, len(population.population) // 30):
