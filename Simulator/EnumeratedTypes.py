e8b2c448e898d1d877d04cb180be28ac6d8d7d92
Elijah Merrill <elmerrill1@icloud.com>
2025-10-07 14:17:00 -0400

    Update EnumeratedTypes.py

--- Simulator/EnumeratedTypes.py
+++ Simulator/EnumeratedTypes.py
@@ -1,13 +1,15 @@
 from enum import Enum, auto
 
-# Different states a person can be in during the simulation. Represents traditional SIR model.
 class State(Enum):
+    ''' Different states a person can be in during the simulation. Represents traditional SIR model.'''
+    
     SUSCEPTIBLE = auto()
     INFECTED = auto()
     RECOVERED = auto()
 
-# Different types of interventions that can be applied in the simulation.
 class InterventionType(Enum):
+    ''' Different types of interventions that can be applied in the simulation.'''
+    
     VACCINE = auto()
     QUARANTINE = auto()
     SOCIAL_DISTANCING = auto()
