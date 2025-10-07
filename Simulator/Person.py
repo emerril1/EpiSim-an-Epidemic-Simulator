4281b35d14d46b61fa1f11a0ba8378a016d8cfdd
Elijah Merrill <elmerrill1@icloud.com>
2025-10-07 14:19:24 -0400

    Update Person.py

--- Simulator/Person.py
+++ Simulator/Person.py
@@ -1,28 +1,28 @@
 from EnumeratedTypes import State
 
-# Class representing a person in the simulation. 
-# Can either be susceptible, infected, or recovered.
-# Functions to infect or cure the person.
-
 class Person:
+    ''' Class representing a person in the simulation. Can either be susceptible, infected, or recovered.
+        Functions to infect or cure the person. '''
+    
     _id = 0
     state = []
 
-    # Initialize a person with a unique ID and susceptible state.
     def __init__(self):
+        ''' Initialize a person with a unique ID and susceptible state.'''
+        
         Person._id += 1
         self.id = Person._id
         self.state = State.SUSCEPTIBLE
         self.infection_time = None
     
-    # Function to infect the person, changing their state from susceptible to infected.
     def infect(self, time: int):
+        ''' Function to infect the person, changing their state from susceptible to infected.'''
         if self.state == State.SUSCEPTIBLE:
             self.state = State.INFECTED
             self.infection_time = time
     
-    # Function to cure the person, changing their state from infected to recovered.
     def cure(self, time: int):
+        ''' Function to cure the person, changing their state from infected to recovered. '''
         if self.state == State.INFECTED:
             self.state = State.RECOVERED
             self.infection_time = time
