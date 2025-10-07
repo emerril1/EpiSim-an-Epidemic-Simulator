5593fb3e89f1d0b5871d936ac66939702d016a25
Elijah Merrill <elmerrill1@icloud.com>
2025-10-07 14:24:09 -0400

    Update Virus.py

--- Simulator/Virus.py
+++ Simulator/Virus.py
@@ -1,8 +1,9 @@
-# Class representing a virus with its properties.
-
 class Virus:
-    # Initialize with name, infection rate, and cure rate.
+    ''' Class representing a virus with its properties.'''
+
     def __init__(self, name: str, infect_rate: float, cure_rate: float):
+        ''' Initialize with name, infection rate, and cure rate.'''
+        
         self.name = name
         self.infect_rate = infect_rate
         self.cure_rate = cure_rate
